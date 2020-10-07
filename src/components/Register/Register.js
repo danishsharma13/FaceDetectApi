c5a29fbb771d7607a3fb7ade94c1308879ccd6bb
Danish Sharma <danishsharma1999@gmail.com>
2020-10-07 01:40:48 -0400

    Updating files App Register and Signin

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends Component {
 	};
 
 	onRegister = () => {
-		fetch('http://localhost:3000/register', {
+		fetch('https://frozen-wave-79143.herokuapp.com/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
